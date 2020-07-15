090551e16fb55f8a4a95c878ef979f80454a552a
Mangaldeep Das <rockonliku88@gmail.com>
2020-07-15 20:51:55 +0530

    Add files via upload

---
+++ src/main/java/in/mangaldeep/MainActivity.java
@@ -0,0 +1,32 @@
+package in.mangaldeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton imageButton;
+    private ConstraintLayout constraintLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.imageButton);
+        constraintLayout = findViewById(R.id.layout);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+                constraintLayout.setBackgroundColor(color);
+            }
+        });
+    }
+}
